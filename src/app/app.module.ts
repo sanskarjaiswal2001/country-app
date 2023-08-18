5078d31e3a17811871436fa6daf0fad5e04216c3
Sanskar Jaiswal <phoenix2810@protonmail.com>
2023-08-18 15:39:11 +0530

    Basic Requirements full-filled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { GridModule } from '@progress/kendo-angular-grid';
 import { AppComponent } from './app.component';
-
+import { CountryTableComponent } from './country-table/country-table.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CountryTableComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonsModule,
+    GridModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/country-table/country-table.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-country-table',
+  templateUrl: './country-table.component.html',
+  styleUrls: ['./country-table.component.css'],
+})
+export class CountryTableComponent implements OnInit {
+  public data: any[] = [];
+  public pageSize = 20;
+  public skip = 0;
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.loadData();
+  }
+  public pageChange(event: any): void {
+    this.skip = event.skip;
+    this.loadData();
+  }
+  private loadData(): void {
+    this.http.get<any[]>('https://restcountries.com/v3/all').subscribe(
+      (data: any[]) => {
+        //Country population sort in descending order
+        this.data = data.sort((a, b) => b.population - a.population);
+        //Country serial number
+        this.data.forEach((country, index) => {
+          country.index = index + 1;
+        });
+        //Country currency list
+        this.data.forEach((country) => {
+          const currencies = country.currencies;
+          const currencyKeys = Object.keys(currencies);
+          const currencyList = currencyKeys.map((key) => {
+            return `${currencies[key].symbol} ${currencies[key].name}`;
+          });
+          country.currencyList = currencyList.join(', ');
+        });
+      },
+      (error: any) => {
+        console.error('Error fetching data: ${error}');
+      }
+    );
+  }
+}
